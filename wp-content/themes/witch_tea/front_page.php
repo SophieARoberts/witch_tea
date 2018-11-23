3c16b1064306e722f7cfa8773f5d66d48a4d62a9
Sophie <sophiearoberts3@gmail.com>
2018-11-23 16:19:32 +0000

    intro and menu updates

--- wp-content/themes/witch_tea/front_page.php
+++ wp-content/themes/witch_tea/front_page.php
@@ -25,9 +25,10 @@
 
 <div class="main">
 	<div class="intro">
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+		<p>A new and unique Boba Tea Bar. Coming to Lancaster city centre 2019.</p>
 	</div>
 	<div class="menu">
+		<h2>Click to view menu</h2>
 	</div>
 <!-- 	<div class="logo">
 	</div>	
